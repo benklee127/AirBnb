f8f37bd41873d1f551bd91d98ae156de2f64fbb8
juniper <benklee127@gmail.com>
2023-03-21 16:23:03 -0400

    auth fix 3

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,11 +1,18 @@
 // backend/routes/api/index.js
 const router = require('express').Router();
-
 const { setTokenCookie, restoreUser, requireAuth } = require('../../utils/auth.js');
 const { User } = require('../../db/models');
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
 
 router.use(restoreUser);
 
+router.use('/session', sessionRouter);
+
+router.use('/users', usersRouter);
+
+
+//TEST ROUTES
 router.post('/test', function (req, res) {
     res.json({ requestBody: req.body });
 });
