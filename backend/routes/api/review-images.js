06063e6f054a53ddcc245c0d629c1c04d72ed77b
juniper <benklee127@gmail.com>
2023-03-27 10:23:44 -0400

    added delete review and delete spot images

--- backend/routes/api/review-images.js
+++ backend/routes/api/review-images.js
@@ -2,6 +2,38 @@ const express = require('express');
 const router = express.Router();
 const { Op } = require("sequelize");
 const { Spot, Review, SpotImage, User, ReviewImage, Booking } = require('../../db/models');
+const { requireAuth } = require('../../utils/auth');
+
+//delete review img
+
+router.delete('/:imageId', requireAuth, async (req, res) => {
+    const imageId = req.params.imageId;
+    const userId = req.user.id;
+
+    const image = await ReviewImage.findByPk(imageId, {
+        include: { model: Review }
+    });
+
+    if (!image) {
+        res.status(404).json({
+            "message": "Review Image couldn't be found",
+            "statusCode": 404
+        })
+    }
+    const imageJSON = image.toJSON();
+    if (imageJSON.Review.userId !== userId) {
+        res.status(404).json({
+            "message": "Review must belong to the current user",
+            "statusCode": 403
+        })
+    }
+    await image.destroy();
+
+    res.status(200).json({
+        "message": "Successfully deleted",
+        "statusCode": 200
+    });
+})
 
 //test router setup
 router.get('/test', function (req, res) {
--- backend/routes/api/spot-images.js
+++ backend/routes/api/spot-images.js
@@ -3,6 +3,35 @@ const router = express.Router();
 const { Op } = require("sequelize");
 const { Spot, Review, SpotImage, User, ReviewImage, Booking } = require('../../db/models');
 
+router.delete('/:imageId', requireAuth, async (req, res) => {
+    const imageId = req.params.imageId;
+    const userId = req.user.id;
+
+    const image = await SpotImage.findByPk(imageId, {
+        include: { model: Spot }
+    });
+
+    if (!image) {
+        res.status(404).json({
+            "message": "Spot Image couldn't be found",
+            "statusCode": 404
+        })
+    }
+    const imageJSON = image.toJSON();
+    if (imageJSON.Spot.userId !== userId) {
+        res.status(404).json({
+            "message": "Spot must belong to the current user",
+            "statusCode": 403
+        })
+    }
+    await image.destroy();
+
+    res.status(200).json({
+        "message": "Successfully deleted",
+        "statusCode": 200
+    });
+})
+
 //test router setup
 router.get('/test', function (req, res) {
     res.send('endpoint hit');
