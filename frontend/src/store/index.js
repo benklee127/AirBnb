9f26b818345ae0541220e28f097b4b5c52dac2e3
juniper <benklee127@gmail.com>
2023-05-08 11:00:50 -0400

    session

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,7 +1,9 @@
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
+import sessionReducer from './session';
 
 const rootReducer = combineReducers({
+    session: sessionReducer,
 });
 
 let enhancer;
@@ -14,6 +16,8 @@ if (process.env.NODE_ENV === 'production') {
         window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
     enhancer = composeEnhancers(applyMiddleware(thunk, logger));
 }
+
+
 
 const configureStore = (preloadedState) => {
     return createStore(rootReducer, preloadedState, enhancer);
